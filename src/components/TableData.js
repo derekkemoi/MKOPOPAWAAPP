194de5e5ee94ae3a95f039c9837b095c9837203a
Kipkemoi Derrick <derekkipkemoi@gmail.com>
2024-10-19 17:08:02 +0300

    Changes

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -86,7 +86,7 @@ export default function TableData(props) {
                     variant="solid"
                     color="success"
                   >
-                    Verfied
+                    Verified
                   </Chip>
                   :
                   <Chip
